f1db6481c4732ba7b69534e908f465826196a432
Admin <Admin@DESKTOP-UJRSTTQ>
2024-11-05 08:52:00 +0530

    Second push

---
+++ FrameworkDemo/src/test/java/contacttest/ToCreateContactTest.java
@@ -0,0 +1,12 @@
+package contacttest;
+
+public class ToCreateContactTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hi");
+
+	}
+
+}
---
+++ FrameworkDemo/src/test/java/organization/ToCreteOrgTest.java
@@ -0,0 +1,11 @@
+package organization;
+
+public class ToCreteOrgTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("bye");
+
+	}
+
+}
